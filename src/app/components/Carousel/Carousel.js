01587b4679d5a54b0359d76ae8f51c05ab535d52
GCMoony <47188474+GCMoony@users.noreply.github.com>
2023-11-30 16:53:34 -0700

    adjusted style

--- src/app/components/Carousel/Carousel.js
+++ src/app/components/Carousel/Carousel.js
@@ -18,22 +18,27 @@ export default function Carousel({children}) {
       setCurrentSlide(allData.length - 1);
   }
 
+  function goDirect(event) {
+    let id = event.target.id.split("scrambleScramble")[1];
+    setCurrentSlide(id);
+  }
+
   let allData = [
     defData,
     defData2,
     defData3
   ]
   return (
-    <div className='bg-slate-300 p-5 rounded block w-screen'>
+    <div className='bg-slate-300 p-5 rounded block'>
       {allData[currentSlide]}
-      <div className="flex justify-between">
-        <button onClick={goBackward}>⬅️</button>
-        <div className="flex gap-3 justify-evenly items-center">
+      <div className="flex justify-between p-4 select-none">
+        <button className="select-none text-2xl flex-1 grayscale opacity-60" onClick={goBackward}>⬅️</button>
+        <div className="flex flex-1 gap-3 justify-evenly items-center select-none ">
           {allData.map((item, key) => {
-            return <div className={"w-5 rounded-full aspect-square " + `${key == currentSlide ? "bg-slate-500" : "bg-slate-400"}`} key={key}></div>
+            return <div id={"scrambleScramble" + key} onClick={goDirect} className={" select-none w-4 rounded-full aspect-square " + `${key == currentSlide ? "bg-slate-500" : "bg-slate-400"}`} key={key}></div>
           })}
         </div>
-        <button onClick={goForward}>➡️</button>
+        <button className="select-none text-2xl flex-1 grayscale opacity-60" onClick={goForward}>➡️</button>
       </div>
     </div>
   )
