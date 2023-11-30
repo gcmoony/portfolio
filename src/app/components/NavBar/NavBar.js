7dbb7b5d9f938acf70f5b434911e7be6eac82c70
GCMoony <47188474+GCMoony@users.noreply.github.com>
2023-11-30 08:54:40 -0700

    Created basic nav bar

---
+++ src/app/components/NavBar/NavBar.js
@@ -0,0 +1,84 @@
+'use client'
+import { useState } from "react";
+
+let links = [
+  {
+    linkName: "Home",
+    linkRoute: "/"
+  },
+  {
+    linkName: "About",
+    linkRoute: "/about"
+  },
+  {
+    linkName: "Something",
+    linkRoute: "/something"
+  },
+];
+
+let linkLayout = links.map((link, key) => {
+  return (
+    <li className="text-lg" key={key}>
+      {/* <a  className="p-2 rounded opacity-80 hover:opacity-100" href={link.linkRoute}>{link.linkName}</a> */}
+      <NavBarLink linkRoute={link.linkRoute} linkName={link.linkName} />
+    </li>
+  )
+})
+
+export default function NavBar() {
+  // const [isMobile, setIsMobile] = useState(window.innerWidth);
+
+  // window.addEventListener("resize", () => {
+  //   setIsMobile(window.innerWidth)
+  // })
+  return (
+    <>
+    {/*isMobile > 600 ? <DesktopNav/> : <MobileNav/>*/}
+      <DesktopNav/>
+    </>
+  );
+}
+
+function MobileNav() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  function openMenu() {
+    setMenuOpen(e => !e);
+  }
+
+
+  return (
+    <nav className=" flex relative justify-between items-center bg-slate-100 p-3">
+      <div className="hover:cursor-pointer text-xl "><a href="/">GCMunoz</a></div>
+      <button className=" rounded-lg text-xl py-2 z-20 hover:bg-slate-500" onClick={openMenu}>
+        {menuOpen ? '📂': '📁'}
+      </button>
+      <ul className={menuOpen ? "transition duration-200 border-l-2 border-slate-300 min-h-screen top-full right-0 w-48 min-w-fit bg-slate-200 absolute" : "w-0 transition duration-200 overflow-hidden absolute"}>
+        {linkLayout}
+      </ul>
+        
+    </nav>
+  );
+}
+
+function DesktopNav() {
+  return (
+    <nav className="flex justify-between py-5 px-4">
+      <div className="hover:cursor-pointer text-2xl"><a href="/">GCMunoz</a></div>
+      <ul className="flex gap-4 justify-end">
+        {links.map((link, key) => {
+          return (
+            <li className="text-lg" key={key}>
+              {/* <a  className="p-2 rounded opacity-80 hover:opacity-100" href={link.linkRoute}>{link.linkName}</a> */}
+              <NavBarLink styles="rounded" linkRoute={link.linkRoute} linkName={link.linkName} />
+            </li>
+          )
+        })}
+      </ul>
+    </nav>
+  );
+}
+
+function NavBarLink({linkRoute, linkName, ...rest}) {
+  return <a  className={"block p-2 hover:bg-slate-300 opacity-80 hover:opacity-100 transition" + " " + rest.styles} href={linkRoute}>{linkName}</a>
+}
