df918e6c00a9930e9715b336e77339af6bf175c3
GCMoony <47188474+GCMoony@users.noreply.github.com>
2025-02-27 02:21:07 +0900

    Added certification component and images

--- src/components/Certifications/Certifications.jsx
+++ src/components/Certifications/Certifications.jsx
@@ -1,73 +1,71 @@
+import "./styles.css"
 export default function Certifications() {
+  const certs = [
+    {
+      name: "Linux Essentials",
+      institute: "Linux Professional Institute",
+      cred_link: "https://lpi.org/v/LPI000598272/ubc36knsgn",
+      image: "./img/certs/lpi-logo.png",
+    },
+    {
+      name: "Security+",
+      institute: "CompTIA",
+      cred_link:
+        "https://www.credly.com/badges/a7cf8875-cd1c-4fd7-8b0a-28e77a9d1228/public_url",
+      image: "./img/certs/security-logo.png",
+    },
+    {
+      name: "A+",
+      institute: "CompTIA",
+      cred_link:
+        "https://www.credly.com/badges/1250bfa3-a3fc-4a8a-b2d3-22badef91492/public_url",
+      image: "./img/certs/a-logo.png",
+    },
+    {
+      name: "ITIL 4 Foundations",
+      institute: "Axelos",
+      cred_link:
+        "https://www.credly.com/badges/fe8b2739-bf7a-485b-ad5a-76478a7a2b1a/public_url",
+      image: "./img/certs/itil4-logo.png",
+    },
+  ]
   return (
     <div
-      id='About'
-      className='section'
+      id='Certifications'
+      className='certifications'
     >
       <h2>IT Certified</h2>
-      <p>I've been officially certified in a couple of things:</p>
-      <ul className='sub list-plain'>
-        <li>
-          <a
-            target='blank'
-            href='https://lpi.org/v/LPI000598272/ubc36knsgn'
-          >
-            <img
-              src='https://github.com/gcmoony/gcmoony/assets/47188474/409b4c7a-3c75-4440-898b-ac7cdc51b36f'
-              alt='Linux Essentials Icon'
-              style={{ display: "block" }}
-            />
-            Linux Essentials
-            <br />
-            Linux Professional Institute
-          </a>
-        </li>
-        <li>
-          <a
-            target='blank'
-            href='https://www.credly.com/badges/a7cf8875-cd1c-4fd7-8b0a-28e77a9d1228/public_url'
-          >
-            <img
-              src='https://github.com/gcmoony/gcmoony/assets/47188474/a69392cc-0fe1-4cc3-8ceb-f93e231961a4'
-              alt='CompTIA Security+ Icon'
-              style={{ display: "block" }}
-            />
-            Security+
-            <br />
-            CompTIA
-          </a>
-        </li>
-        <li>
-          <a
-            target='blank'
-            href='https://www.credly.com/badges/1250bfa3-a3fc-4a8a-b2d3-22badef91492/public_url'
-          >
-            <img
-              src='https://github.com/gcmoony/gcmoony/assets/47188474/5f3cfe3d-fc19-42d0-af47-5cdd53e54808'
-              alt='CompTIA A+ Icon'
-              style={{ display: "block" }}
-            />
-            A+
-            <br />
-            CompTIA
-          </a>
-        </li>
-        <li>
-          <a
-            target='blank'
-            href='https://www.credly.com/badges/fe8b2739-bf7a-485b-ad5a-76478a7a2b1a/public_url'
-          >
-            <img
-              src='https://github.com/gcmoony/gcmoony/assets/47188474/f6c3ca18-fe67-46a0-a1ff-ff16145a3dea'
-              alt='ITIL 4 Icon'
-              style={{ display: "block" }}
-            />
-            ITIL 4 Foundations
-            <br />
-            Axelos
-          </a>
-        </li>
-      </ul>
+      <p>
+        Along my career path, I've studied different topics in technology and
+        IT. Check out some of the certificates I've earned.
+      </p>
+      <div className='container'>
+        {certs.map((cert, index) => {
+          return (
+            <div
+              className='card'
+              key={index}
+            >
+              <img
+                src={cert.image}
+                alt={`${cert.name} icon`}
+              />
+              <h3>
+                {cert.name}
+                <br />
+              </h3>
+              <p>
+                <em>{cert.institute}</em>
+              </p>
+              <a
+                target='_blank'
+                rel='noreferrer'
+                href={cert.cred_link}
+              ></a>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
