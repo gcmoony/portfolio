2fca1420d553ad257cf21c60d754726cfbdc0808
GCMoony <47188474+GCMoony@users.noreply.github.com>
2026-02-20 06:47:35 -0500

    Add region for nav

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,24 +1,25 @@
-import "./styles.css"
+import "./styles.css";
 
 export default function Navigation() {
   return (
-    <div className='Nav-Bar'>
+    <header className='Nav-Bar'>
       <h1 className='Nav-Logo'>GCMunoz</h1>
-
-      <ul className='Nav-Links'>
-        <li>
-          <a href='#About'>About</a>
-        </li>
-        <li>
-          <a href='#Projects'>Projects</a>
-        </li>
-        <li>
-          <a href='#Certifications'>Certifications</a>
-        </li>
-        <li>
-          <a href='#Contact'>Contact</a>
-        </li>
-      </ul>
-    </div>
-  )
+      <nav>
+        <ul className='Nav-Links'>
+          <li>
+            <a href='#About'>About</a>
+          </li>
+          <li>
+            <a href='#Projects'>Projects</a>
+          </li>
+          <li>
+            <a href='#Certifications'>Certifications</a>
+          </li>
+          <li>
+            <a href='#Contact'>Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
